97e1cfcf8347fd7fcfd506e68765fc688489bab6
LeeJoeIng <45207734+LeeJoeIng@users.noreply.github.com>
2022-01-26 10:10:31 +0100

    Update mqtt_rpi_car.py

--- mqtt_rpi_car.py
+++ mqtt_rpi_car.py
@@ -1,105 +1,97 @@
+#!/usr/bin/env python3
+
+import json
+import time
+import pandas as pd
 import paho.mqtt.publish as publish
 
-import time
-import random
-
-#broker_address = "137.194.218.9"
 broker_address = "test.mosquitto.org"
 
 # publish messages on these topics
-pub_topic1 = "sensor/temperature"
-pub_topic2 = "sensor/humidity"
-pub_topic3 = "sensor/pressure"
-pub_topic4 = "sensor/water"
-pub_topic5 = "sensor/light"
-pub_topic6 = "sensor/wind"
-pub_topic7 = "sensor/tire"
-pub_topic8 = "sensor/vitesse"
-pub_topic9 = "sensor/fire"
+pub_topic1 = "rio203/accelerationFromJSON"
+pub_topic2 = "rio203/latitudeFromJSON"
+pub_topic3 = "rio203/longitudeFromJSON"
+pub_topic4 = "rio203/speedFromJSON"
+pub_topic5 = "rio203/status"
 
-# # # # # # # # # # # # # # # GPIO inputs # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # Preparing payload # # # # # # # # # # # # # # # # # #
 
-def read_temp():
-   temperature = str(random.randint(0,100)) #random modifier
+def read_accel(i):
+   accel = dataframe.loc[i,'acceleration']
    payload="{"
-   payload+="\"Temperature\":"
-   payload+=temperature
+   payload+="\"Acceleration\":"
+   payload+=str(accel)
    payload+="}"
+   #print(payload)
    return payload
 
-def read_humidity():
-   humidity = str(random.randint(0,100)) #random modifier
+def read_speed(i):
+   speed = dataframe.loc[i,'speed']
    payload="{"
-   payload+="\"Humidity\":"
-   payload+=humidity
+   payload+="\"Speed\":"
+   payload+=str(speed)
    payload+="}"
+   #print(payload)
    return payload
 
-def read_pressure():
-   pressure = str(random.randint(0,100)) #random modifier
+def read_latitude(i):
+   latitude = dataframe.loc[i,'latlng'][0]
    payload="{"
-   payload+="\"Pressure\":"
-   payload+=pressure
+   payload+="\"Latitude\":"
+   payload+=str(latitude)
    payload+="}"
+   #print(payload)
    return payload
 
-def read_water():
-   water = str(random.randint(0,100)) #random modifier
+def read_longtitude(i):
+   longtitude = dataframe.loc[i,'latlng'][1]
    payload="{"
-   payload+="\"Water\":"
-   payload+=water
+   payload+="\"Longtitude\":"
+   payload+=str(longtitude)
    payload+="}"
+   #print(payload)
    return payload
 
-def read_light():
-   light = str(random.randint(0,100)) #random modifier
+def read_status(str):
+   status = str
    payload="{"
-   payload+="\"Light\":"
-   payload+=light
+   payload+="\"Status\":"
+   payload+=status
    payload+="}"
+   #print(payload)
    return payload
+# # # # # # # # # # # # # # # JSON inputs # # # # # # # # # # # # # # # # # #
 
-def read_wind():
-   wind = str(random.randint(0,100)) #random modifier
-   payload="{"
-   payload+="\"Wind\":"
-   payload+=wind
-   payload+="}"
-   return payload
+#read json file
+with open("trajet.json") as jsonFile:
+    jsonObject = json.load(jsonFile)
+    jsonFile.close()
 
-def read_tire():
-   tire = str(random.randint(0,100)) #random modifier
-   payload="{"
-   payload+="\"Tire\":"
-   payload+=tire
-   payload+="}"
-   return payload
+#read data from json file
+data = jsonObject['data'][0]
+dataFields = data['fields']
+dataValues = data['values']
 
-def read_vitesse():
-   vitesse = str(random.randint(0,100)) #random modifier
-   payload="{"
-   payload+="\"Vitesse\":"
-   payload+=vitesse
-   payload+="}"
-   return payload
+#create dataframe
+dataframe = pd.DataFrame(data=dataValues, index=list(range(len(dataValues))), columns=dataFields)
+dataframe['time']=pd.to_datetime(dataframe['time'],unit='s')
 
-def read_fire():
-   fire = str(random.randint(0,100)) #random modifier
-   payload="{"
-   payload+="\"Fire\":"
-   payload+=fire
-   payload+="}"
-   return payload
+dataframe['acceleration'] = dataframe['speed'].diff() #car il y a un relevé par seconde et la vitesse en en m/s
+dataframe.loc[dataframe['acceleration'].isna()==True,'acceleration']=0 #Vérifie qu'aucune valeur ne soit NAN
 
-while True:
-    publish.single(pub_topic1, read_temp(), hostname = broker_address)
-    publish.single(pub_topic2, read_humidity(), hostname = broker_address)
-    publish.single(pub_topic3, read_pressure(), hostname = broker_address)
-    publish.single(pub_topic4, read_water(), hostname = broker_address)
-    publish.single(pub_topic5, read_light(), hostname = broker_address)
-    publish.single(pub_topic6, read_wind(), hostname = broker_address)
-    publish.single(pub_topic7, read_tire(), hostname = broker_address)
-    publish.single(pub_topic8, read_vitesse(), hostname = broker_address)
-    publish.single(pub_topic9, read_fire(), hostname = broker_address)
+#mention that the session is started
+publish.single(pub_topic5, read_status("On route"), hostname = broker_address)
+
+#publish messages to Rpi server
+for j in range(len(dataframe['acceleration'])) :
+#for j in range(5) :
+    publish.single(pub_topic1, read_accel(j), hostname = broker_address)
+    publish.single(pub_topic2, read_latitude(j), hostname = broker_address)
+    publish.single(pub_topic3, read_longtitude(j), hostname = broker_address)
+    publish.single(pub_topic4, read_speed(j), hostname = broker_address)
+
     print("Done")
-    time.sleep(10)
+#    time.sleep(1)
+
+#mention that the session is over
+publish.single(pub_topic5, read_status("Stop"), hostname = broker_address)
