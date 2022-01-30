c783e129f98d0f05189a0b9e68c3e23bda49be6e
Karen Caloyannis <karen.caloyannis@gmail.com>
2022-01-30 13:53:35 +0100

    entretien v2

---
+++ entretienv2/km_predict.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 30 12:40:58 2022
+
+@author: karencaloyannis
+"""
+import numpy as np
+import requests
+
+from datetime import datetime, timedelta
+
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+############# RECUPERATION DES VALEURS STOCKEES SUR THINGSBOARD #############
+# Header
+headers = {
+    'Content-type': 'application/json',
+    'X-Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJ1c2VySWQiOiI5ZjBiMTUzMC02ZTc3LTExZWMtYWM2NC04OWI1N2RkODVhYzEiLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiOWQ3NjFkNTAtNmU3Ny0xMWVjLWFjNjQtODliNTdkZDg1YWMxIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNjQzNTQxODk4LCJleHAiOjE2NDM1NTA4OTh9.t_RlBwT_FdxrTHGPfRTyf2kz-RwcibNb838imN4HJwfOvN9EW6usAN9aplU7ObYLspzgyszkZUeVthoaOyQBQQ',
+}
+
+# URL pour récupérer la date de la dernière révision et son kilométrage
+url_revision = 'http://localhost:8080/api/plugins/telemetry/DEVICE/4f5afac0-70a5-11ec-a326-4345504f184b/values/attributes/SHARED_SCOPE?keys=date_derniere_revision'
+
+response = requests.get(url_revision, headers=headers)
+response_json = response.json()
+
+# Parser la réponse
+response_json = response.json() 
+
+for key in response_json : 
+    date_derniere_revision = int(key['value']) # Récupération de la date de dernière révision
+
+# URL pour les kilométrages à récupérer 
+# commande test sur terminal :
+    # curl -v -X GET 'http://localhost:8080/api/plugins/telemetry/DEVICE/4f5afac0-70a5-11ec-a326-4345504f184b/values/timeseries?keys=kilometrage&startTs=1640991599000&endTs=1643151599000&agg=NONE' --header 'Content-Type:application/json' --header 'X-Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJ1c2VySWQiOiI5ZjBiMTUzMC02ZTc3LTExZWMtYWM2NC04OWI1N2RkODVhYzEiLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiOWQ3NjFkNTAtNmU3Ny0xMWVjLWFjNjQtODliNTdkZDg1YWMxIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNjQzNTQxODk4LCJleHAiOjE2NDM1NTA4OTh9.t_RlBwT_FdxrTHGPfRTyf2kz-RwcibNb838imN4HJwfOvN9EW6usAN9aplU7ObYLspzgyszkZUeVthoaOyQBQQ'
+# On récupère les kilométrages depuis la dernière révision
+url_recup = 'http://localhost:8080/api/plugins/telemetry/DEVICE/4f5afac0-70a5-11ec-a326-4345504f184b/values/timeseries?keys=kilometrage&startTs=' + str(date_derniere_revision) + '&endTs=1643151599000&agg=NONE'
+#url_recup =  'http://localhost:8080/api/plugins/telemetry/DEVICE/4f5afac0-70a5-11ec-a326-4345504f184b/values/timeseries?keys=kilometrage&startTs=1640991599000&endTs=1643151599000&agg=NONE'
+
+response = requests.get(url_recup, headers=headers)
+
+# Parser la réponse
+response_json = response.json() 
+
+ts = [] #timestamps
+km = [] #kilométrage
+
+for key in response_json['kilometrage']:
+    ts.append(int(key['ts'])/1000)
+    km.append(float(key['value']))
+
+ts = np.array(ts)
+km = np.array(km)
+km_derniere_revision = np.zeros((1,))
+km_derniere_revision[0] = km[0]
+print(km_derniere_revision)
+
+############## REGRESSION KILOMETRAGE ###################
+
+degree=1 #Dregee du polynome pour la régression
+
+# modèle prédiction de la date en fonction du kilométrage
+model_date_pred = make_pipeline(PolynomialFeatures(degree), Ridge(alpha=1e-3))
+model_date_pred.fit(km[:, np.newaxis], ts)
+
+# Prédiction de la date de prochaine révision (on fait une révision tous les 15 000km)
+date_revision_pred = model_date_pred.predict(km_derniere_revision[:, np.newaxis]+15000) 
+print(date_revision_pred)
+next_service = datetime.fromtimestamp(date_revision_pred[0])
+print("prochaine révision:"+str(next_service))
+
+# Envoi de la date de prochaine révision vers Thingsboard 
+url = 'http://localhost:8080/api/v1/vmWsSYMqGg8AGCiamhM9/telemetry'
+data = '{"next_service":' + str(int(date_revision_pred[0])) + '}'
+response = requests.post(url, headers=headers, data=data)
+
+
+############# PREDICTION DU KILOMETRAGE EN FONCTION DE LA DATE ##############
+# modèle prédiction du kilométrage en fonction de la date
+model_km_pred = make_pipeline(PolynomialFeatures(degree), Ridge(alpha=1e-3))
+model_km_pred.fit(ts[:, np.newaxis], km)
+# Dates dont on veut prédire le kilométrage
+dates_pred = np.arange(datetime.fromtimestamp(ts[0]), datetime.fromtimestamp(date_revision_pred[0]), timedelta(days=1)).astype(datetime)
+#Conversion en timestamps 
+dates_pred_timestamps = []
+for i in range(len(dates_pred)):  
+    dates_pred_timestamps.append(int(datetime.timestamp(dates_pred[i])))
+dates_pred_timestamps = np.array(ts).astype(int)
+
+# kilometrage prédit
+km_pred = model_km_pred.predict(dates_pred_timestamps[:, np.newaxis])
+
+for i in range(len(km_pred)):
+    data = '{'+'\"ts\":' + str(int(dates_pred_timestamps[i])) + "000," + '\"values\":{\"kilometrage_pred\":' + str(km_pred[i]) + '}}'
+    response = requests.post(url, headers=headers, data=data)
---
+++ entretienv2/km_sim.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 30 12:40:47 2022
+
+@author: karencaloyannis
+"""
+
+import numpy as np
+import time
+import requests
+
+from datetime import datetime, timedelta
+
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+# préparation pour envoi vers thingsboard
+# Header
+headers = {
+    'Content-type': 'application/json',
+}
+# URL thingsboard pour le device "Maintenance"
+url = 'http://localhost:8080/api/v1/vmWsSYMqGg8AGCiamhM9/telemetry'
+
+
+################## GENERATION DES DONNEES DE SIMULATION ###################
+## Dates simulées pour le kilométrage
+# On suppose qu'on commence les mesures à partir de la date de la derniere revision 
+dates = np.arange(datetime(2021,1,1,23,59,59), datetime(2022,1,26,23,59,59), timedelta(days=1)).astype(datetime)
+date_timestamps = []
+for i in range(len(dates)):  
+    date_timestamps.append(int(datetime.timestamp(dates[i])))
+date_timestamps = np.array(date_timestamps).astype(int)
+
+## simulation du kilométrage
+km = np.random.normal(loc=80,scale=50,size=(len(dates),))
+km_initial = 0
+km = np.cumsum(km)
+km = km + km_initial
+
+# Envoi du kilometrage a thingsboard
+for i in range(len(km)):
+    #time.sleep(1)
+    data = '{'+'\"ts\":' + str(int(date_timestamps[i])) + "000," + '\"values\":{\"kilometrage\":' + str(km[i]) + '}}'
+    response = requests.post(url, headers=headers, data=data)
+
