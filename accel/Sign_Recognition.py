f91ee396a3b662655ef5840dfb80e02e663bd712
Americium92 <56517067+Americium92@users.noreply.github.com>
2022-01-31 22:13:58 +0100

    modification of stop detection

--- accel/Sign_Recognition.py
+++ accel/Sign_Recognition.py
@@ -13,62 +13,98 @@ from vidgear.gears import CamGear
 import cv2
 import numpy as np
 import time
+import datetime
 
 Stop_cascade = cv2.CascadeClassifier('Stop_classificateur.xml')
-Pieton_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 
 #stream = CamGear(source='https://youtu.be/Nbira-ZTPq0', stream_mode = True, logging=True).start() # YouTube Video URL as input
-stream = cv2.VideoCapture ("pane.mp4")
+stream = cv2.VideoCapture("pane.mp4")
+
+if not stream.isOpened:
+    print('--(!)Error opening video capture')
+    exit(0)
+
 font = cv2.FONT_HERSHEY_SIMPLEX
+
+temps = 0
+temps_entier = 0
+entier_precedent = 0
+presence_stop = 0
+tempsActuel = 0
+t = 0.0
+tempsPrecedent = 0
+tabStop = []
+
+t0 = time.time()
+
 
 # infinite loop
 while True:
+
+
+    tempsActuel = time.time()
     ret, frame = stream.read()
-    frame = cv2.resize(frame,(740,460))
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # read frames
 
-    # check if frame is None
+
+    fps = 1/(tempsActuel-tempsPrecedent)
+    multiplicateur = fps/30
+
+    t_transition = tempsActuel-tempsPrecedent
+    temps += (tempsActuel-tempsPrecedent)*multiplicateur
+    temps
+
+    tempsPrecedent = tempsActuel
+    fps = int(fps)
+
+    #print("temps : " + str(temps) + " fps : " + str(fps))
+
     if frame is None:
-        #if True break the infinite loop
+        print('--(!) No captured frame -- Break!')
         break
 
-    stop = Stop_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in stop:
-        cv2.putText (frame, "Stop",(x,y),
-                font, 1,
-                (0,255,0),
-                2,
-                cv2.LINE_4)
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-        stop = frame[y:y+h, x:x+w]
-        print("Lewis, stop the car")
-        cv2.imshow('panneau STOP', stop)
 
-    # Car detection
-    pieton = Pieton_cascade.detectMultiScale(gray, 1.3, 3)
-    for (x,y,w,h) in pieton:
-        cv2.putText (frame, "Pieton", (x,y),
-                font, 1,
-                (0,255,0),
-                2,
-                cv2.LINE_4)
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-        pieton = frame[y:y+h, x:x+w]
-        print("Pieton")
-        cv2.imshow('Pieton devant', pieton)
+    if ret:
+        frame = cv2.resize(frame,(720,480))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.putText (frame, str(temps) + " : " + str(fps),(40,40),
+        font, 1,
+        (0,0,0),
+        2,
+        cv2.LINE_4)
 
-    cv2.imshow("Output Frame", frame)
-    # Show output window
+        stop = Stop_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in stop:
+            cv2.putText (frame, "Stop",(x,y),
+                    font, 1,
+                    (0,255,0),
+                    2,
+                    cv2.LINE_4)
+            frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            stop = frame[y:y+h, x:x+w]
+            presence_stop = 1
+            cv2.imshow('panneau STOP', stop)
 
-    key = cv2.waitKey(1) & 0xFF
-    # check for 'q' key-press
-    if key == ord("q"):
-        #if 'q' key-pressed break out
-        break
+        cv2.imshow("Output Frame", frame)
+        # Show output window
 
+        key = cv2.waitKey(1) & 0xFF
+        # check for 'q' key-press
+        if key == ord("q"):
+            #if 'q' key-pressed break out
+            break
+
+        temps_entier = int(temps)
+        if(entier_precedent != temps_entier):
+            tabStop.append(presence_stop)
+        entier_precedent = temps_entier
+
+
+
+
+
+    presence_stop = 0
+
+print(tabStop)
+print(len(tabStop))
 cv2.destroyAllWindows()
 # close output window
-
-# safely close video stream.
-stream.stop()
